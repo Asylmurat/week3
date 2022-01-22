d1981e0ee09544bd2bc58e4b77119dd0de868aed
Asylmurat <73055112+Asylmurat@users.noreply.github.com>
2022-01-22 13:29:45 +0600

    2

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,11 +1,34 @@
-package com.example.singleton
-//pattern singleton
+package com.example.a2
+//pattern builder
 fun main() {
-    println("Number1 + Number2 = ${Numberplusnumber.add(3,2)}")
+    Builder1("Алекс").createHead()
+    Builder2(2).createHand()
+    Builder3(2).createLeg()
 }
-object  Numberplusnumber {
-    fun add(num1:Int,num2:Int):Int
-    {
-        return num1.plus(num2)
+class Builder1(val name: String) {
+    fun createHead() {
+        println("[]")
+    }
+}
+class Builder2(val hand: Int) {
+    fun createHand() {
+        if (hand == 2) {
+            println("-|-")
+        } else if (hand == 1) {
+            println("-  ")
+        } else {
+            println("Нет")
+        }
+    }
+}
+class Builder3(val leg: Int) {
+    fun createLeg() {
+        if (leg == 2) {
+            println("||")
+        } else if (leg == 1) {
+            println("|  ")
+        } else {
+            println("Нет")
+        }
     }
 }
